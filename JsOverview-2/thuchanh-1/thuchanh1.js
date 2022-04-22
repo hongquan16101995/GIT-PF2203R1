dc1411501df35b173e137ed2ecf5114cd441732b
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2022-04-22 21:47:30 +0700

    dom buoi 1

--- JsOverview-2/thuchanh-1/thuchanh1.js
+++ JsOverview-2/thuchanh-1/thuchanh1.js
@@ -1,3 +1,9 @@
 function showMessage() {
-    alert('Xin chào!')
+    // alert('Xin chào!')
+    let demoConfirm = confirm("Đây là confirm?")
+    if (demoConfirm === true) {
+        alert("giá trị của confirm là true")
+    } else {
+        alert("giá trị của confirm là false")
+    }
 }
