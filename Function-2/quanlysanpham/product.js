7976662eadb4541151c63e3abcdd9863a015b3dc
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2022-06-10 21:21:31 +0700

    va cham

---
+++ Function-2/quanlysanpham/product.js
@@ -0,0 +1,13 @@
+class Product {
+    name
+    price
+    brand
+    description
+
+    constructor(name, price, brand, description) {
+        this.name = name
+        this.price = price
+        this.brand = brand
+        this.description = description
+    }
+}
--- Function-2/quanlysanpham/quanlysanpham.js
+++ Function-2/quanlysanpham/quanlysanpham.js
@@ -1,12 +1,21 @@
-let products = ["Sony Xperia", "Samsung Galaxy", "Nokia 6", "Iphone 11 Pro Max"]
+let product1 = new Product("Sony Xperia", 10000000, "Sony", "Đẹp")
+let product2 = new Product("Samsung Galaxy", 30000000, "Samsung", "Đẹp")
+let product3 = new Product("Nokia 6", 20000000, "Nokia", "Đẹp")
+let product4 = new Product("Iphone 11 Pro Max", 40000000, "Apple", "Đẹp")
+let products = [product1, product2, product3, product4]
 
 function render() {
     let data = "<table>"
-    data += "<tr><td><b>STT</b></td><td><b>Product Name</b></td><td></td><td><b id='red'>"+ products.length +" products</b></td></tr>"
+    data += "<tr><td><b>STT</b></td><td><b>Product Name</b></td><td><b>Product Price</b></td>" +
+    "<td><b>Product Brand</b></td><td><b>Product Description</b></td><td></td>" +
+    "<td><b id='red'>"+ products.length +" products</b></td></tr>"
     for(let i = 0; i < products.length; i++) {
         data += "<tr>"
         data += "<td>" + (i + 1) + "</td>"
-        data += "<td>" + products[i] + "</td>"
+        data += "<td>" + products[i].name + "</td>"
+        data += "<td>" + products[i].price + "</td>"
+        data += "<td>" + products[i].brand + "</td>"
+        data += "<td>" + products[i].description + "</td>"
         data += "<td><button onclick='editProduct("+i+")'>Edit</button></td>"
         data += "<td><button onclick='deleteProduct("+i+")'>Delete</button></td>"
         data += "</tr>"
@@ -16,7 +25,7 @@ function render() {
 }
 
 function deleteProduct(index) {
-    if (confirm("Bạn có chắc chắn muốn xóa sản phẩm có tên: " +  products[index] + " ?")) {
+    if (confirm("Bạn có chắc chắn muốn xóa sản phẩm có tên: " +  products[index].name + " ?")) {
          products.splice(index, 1)
     }
     render()
@@ -33,25 +42,38 @@ let indexEdit;
 //edit với ô input
 function editProduct(index) {
     indexEdit = index
-    document.getElementById("product").value = products[index]
+    document.getElementById("name").value = products[index].name
+    document.getElementById("price").value = products[index].price
+    document.getElementById("brand").value = products[index].brand
+    document.getElementById("description").value = products[index].description
     document.getElementById("add").value = "Edit"
     document.getElementById("add").setAttribute("onclick", "changeValueProductEdit()")
 }
 
 function changeValueProductEdit() {
-    let updateProduct = document.getElementById("product").value
-    products[indexEdit] = updateProduct
-    document.getElementById("product").value = ""
+    let name = document.getElementById("name").value
+    let price = document.getElementById("price").value
+    let brand = document.getElementById("brand").value
+    let description = document.getElementById("description").value
+    let product = new Product(name, price, brand, description)
+    products[indexEdit] = product
     document.getElementById("add").value = "Add"
     document.getElementById("add").setAttribute("onclick", "addProduct()")
     render()
 }
 
 function addProduct(){
-    let newProduct = document.getElementById("product").value
-    products.push(newProduct)
-    document.getElementById("product").value = ""
-    render()
-}
+        let name = document.getElementById("name").value
+        let price = document.getElementById("price").value
+        let brand = document.getElementById("brand").value
+        let description = document.getElementById("description").value
+        let product = new Product(name, price, brand, description)
+        products.push(product)
+        render()
+    }
 
 render()
+
+// //lưu object trong localStorage
+// localStorage.setItem("product", JSON.stringify(product1))
+// let a = JSON.parse(localStorage.getItem("product"))
